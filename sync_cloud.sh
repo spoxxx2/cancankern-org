5a501bf02872a37fd8f7f06ec1450d83d438e76d
Spoxxx2 <cancan@cancankern.org>
2026-01-23 19:23:46 -0800

    ZENITH: Full Site Sections Complete

--- sync_cloud.sh
+++ sync_cloud.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
+# Hardwired Cloud Sync Tunnel
 export PGPASSWORD='37YYNDOhnXwFgTFl'
-# Direct column mapping for 100-year forecast
-psql -h db.dzrnwzvizztfmjgvpajd.supabase.co -p 5432 -d postgres -U postgres -q -c "INSERT INTO debris_logs (event_id, spectral_calibration, objects) SELECT j->>'event_id', j->'spectral_calibration', j->'objects' FROM (SELECT '$1'::jsonb AS j) AS t;" > /dev/null 2>&1
+psql -h db.dzrnwzvizztfmjgvpajd.supabase.co -p 5432 -d postgres -U postgres -c "INSERT INTO debris_logs (event_id, spectral_calibration, objects) SELECT j->>'event_id', j->'spectral_calibration', j->'objects' FROM (SELECT '$1'::jsonb AS j) AS t;"
